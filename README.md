682560f4a4cc262fdcc804edc7f4541bcebd1f4a
noviceGuru <amin.arabi@gmail.com>
2023-08-21 19:25:05 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Description
+
+An Algorithm Exercise. It takes solved Sudoku puzzles in a 2d array and checks them.
---
+++ app.js
@@ -0,0 +1,7 @@
+const isSudokuCorrect = (sudoku) => {
+	// Check if all the rows are made of unique arrays
+	
+	// Check if all the columns are made of unique arrays
+	
+	// Check if all the squares are made of unique arrays
+}
